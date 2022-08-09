1ade66fa12cbb1cad0af5184fbc403d832528346
RidwendDev <akmalridwanmabaugm@gmail.com>
2022-08-09 20:24:29 +0700

    menambahkan sg1

---
+++ styleGuide1.py
@@ -0,0 +1,29 @@
+# Yg pertama itu ada yg namanya **hanging indent** dimana rata kiri dg kurung/pemisah arg. awal
+
+def namaPanjang(namaDepan, 
+                namaBelakang):
+    namaDepan = namaDepan
+    namaBelakang = namaBelakang
+    print(namaDepan+' '+namaBelakang)
+
+namaPanjang('bambang','sutejo')
+
+# kalo nda biasnya kita juga bisa lakukan spt ini
+def long_function_name(
+        var_one, var_two, var_three,
+        var_four):
+    print(var_one)
+
+# masalah kurung siku
+my_list = [
+    1, 2, 3, 7, 8, 9,
+    4, 5, 6, 10, 11, 12
+    ]
+
+# utk import lib biasanya urutan spt ini:
+'''
+1. Standard Library
+2. Library Pihak Ketiga
+3. Local/Library spesifik
+'''
+
